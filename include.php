1549cff96b4c40c3d4e1a00e3a6aa195c9f93214
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-14 20:14:49 +0100

    Added default include.php and temporary index.php

---
+++ include.php
@@ -0,0 +1,36 @@
+<?php
+// THIS FILE IS PART OF THE MOODJI PROJECT
+// Created by moriczgergo
+// Creation date: 2017.01.14
+// Last edited: 2017.01.14
+?>
+<html>
+	<head>
+		<!-- Setting charset to UTF-8 !-->
+		<meta charset="UTF-8">
+
+		<!-- OPTIONAL: Turn off cache !-->
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+
+		<!-- Bootstrap 4 CDN !-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+		<!-- Website title (displays on website's tab in browser) !-->
+		<title>MOODJI</title>
+	</head>
+	<body>
+		<center> <!-- CENTER BEGIN !-->
+			<h1>MOODJI</h1> <!-- Title !-->
+		</center> <!-- CENTER END !-->
+		<hr> <!-- Horizontal line !-->
+
+		<!-- Bootstrap 4 CDN !-->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// THIS FILE IS PART OF THE MOODJI PROJECT
+// Created by moriczgergo
+// Creation date: 2017.01.14
+// Last edited: 2017.01.14
+include "include.php";
+?>
+<html>
+	<body>
+		<h2>Work in progress.</h2>
+	</body>
+</html>
