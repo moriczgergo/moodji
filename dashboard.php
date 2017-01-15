55d3bcf4956232c012c9c5077956f3c20efb043d
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-15 17:35:35 +0100

    Added missing printError() from dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -30,6 +30,24 @@ if($stmt->num_rows == 1){
 }
 
 $mood = base64_decode($selectmood);
+
+function printError($error, $ext = ""){
+	if ($error == "fewval"){
+		echo "<center><h1>You didn't enter all the required data</h1></center>";
+	} elseif ($error == "connerr") {
+		echo "<center><h1>Connection to database failed:</h1><h3>" . $ext . "</h3></center>";
+	} elseif ($error == "unametaken"){
+		echo "<center><h1>Username " . $ext . " already taken.</h1></center>";
+	} elseif ($error == "lastfail"){
+		echo "<center><h1>Something went wrong:</h1><h3>" . $ext . "</h3></center>";
+	} elseif ($error == "unknown"){
+		echo "<center><h1>Something went wrong.</h1></center>";
+	} elseif ($error == "accnotfound"){
+		echo "<center><h1>Account not found.</h1></center>";
+	} elseif ($error == "wrongpass") {
+		echo "<center><h1>Invalid password.</h1></center>";
+	}
+}
 ?>
 <html>
 	<body>
