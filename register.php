19a3618a0a43dc98c16ab7b7db90e942bd20f216
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-15 12:15:57 +0100

    Added register mechanism

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+include "include.php";
+?>
+<html>
+	<body>
+		<center>
+			<h1>Registration</h1>
+			<form action="register_backplane.php" method="POST">
+				<h3>Username</h3>
+				<input type="text" name="user">
+				<h3>Password</h3>
+				<input type="text" name="pass">
+				<h3>E-Mail</h3>
+				<input type="text" name="mail">
+			</form>
+		</center>
+	</body>
+</html>
---
+++ register_backplane.php
@@ -0,0 +1,64 @@
+<?php
+include "include.php";
+include "mysql.php";
+
+if (isset($_POST["user"]) && isset($_POST["pass"]) && isset($_POST["mail"]))
+{
+	if (strlen(trim($_POST["user"])) > 0 && strlen(trim($_POST["pass"])) > 0 && strlen(trim($_POST["mail"])) > 0)
+	{
+		$user = $_POST["user"];
+		$pass = $_POST["pass"];
+		$mail = $_POST["mail"];
+
+		$conn = new mysqli($sqlserv, $sqluser, $sqlpass, $sqldata);
+
+		if ($conn->connect_error) {
+			printError("connerr", $conn->connect_error)
+    		die();
+		}
+
+		$stmt = $conn->prepare("SELECT username FROM users WHERE username=?");
+		$stmt->bind_param("s", $user);
+		$result = $stmt->execute();
+
+		if($result->num_rows == 0){
+			$pass_hash = password_hash($pass, PASSWORD_DEFAULT);
+
+			$stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+			$stmt->bind_param("sss", $user, $pass_hash, $mail);
+			$result = $stmt->execute();
+
+			if ($result === TRUE){
+				printSuccess();
+			} else {
+				printError("lastfail", $conn->error);
+			}
+		} else {
+			printError("unametaken", $user);
+			die();
+		}
+	} else {
+		printError("fewval");
+		die();
+	}
+} else {
+	printError("fewval");
+	die();
+}
+
+function printError($error, $ext = ""){
+	if ($error == "fewval"){
+		echo "<center><h1>You didn't enter all the required data</h1></center>";
+	} elseif ($error == "connerr") {
+		echo "<center><h1>Connection to database failed:</h1><h3>" . $ext . "</h3></center>";
+	} elseif ($error = "unametaken"){
+		echo "<center><h1>Username " . $ext . " already taken.</h1></center>";
+	} elseif ($error = "lastfail"){
+		echo "<center><h1>Something went wrong:</h1><h3>" . $ext . "</h3></center>";
+	}
+}
+
+function printSuccess(){
+	echo "<center><h1>Success!</h1><p><a href=\"login.php\">Click here to login</a></p></center>";
+}
+?>
